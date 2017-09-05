dc533feda99e1518fc76ab638b0d156ed55848fb
mmichalskey <mmichalskey@gmail.com>
2017-09-05 22:44:05 +0200

    test for sfml
    test for sfml

--- PixelHero/PixelHero.cpp
+++ PixelHero/PixelHero.cpp
@@ -1,11 +1,28 @@
-// PixelHero.cpp: Definiuje punkt wejścia dla aplikacji konsolowej.
-//
-
 #include "stdafx.h"
+#include <SFML/Graphics.hpp>
 
+
+//This is only test if sfml library works
 
 int main()
 {
-    return 0;
-}
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
